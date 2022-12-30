e3c36b5dc5880bc20ce7b266e570591f9ab30a0c
Aditya <aditya.pandey.241103@gmail.com>
2022-12-30 18:59:38 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Cards from './components/Cards';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Info from './components/Info';
 import Navbar from './components/Navbar';
@@ -12,7 +13,7 @@ const App = () => (
     <Info />
     <Cards />
     <Venture />
-
+    <Footer />
   </div>
 );
 
