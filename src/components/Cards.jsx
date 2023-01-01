9aa6e682a0ea0bf4191b00652939f2762652ec4d
Aditya <aditya.pandey.241103@gmail.com>
2023-01-01 11:43:08 +0530

    Responsiveness fixes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,12 +11,12 @@ const Cards = () => (
         <div className="container px-5 py-16 mx-auto">
             <div className="flex flex-col text-center w-full mb-20">
                 <h1 className="text-4xl font-light tracking-tight leading-none md:text-5xl xl:text-6xl title-font mb-4 text-gray-900"><span className='text-[#4d55bb] font-bold'>What</span> we do</h1>
-                <p className=" ml-0 md:ml-52 mr-0 md:mr-52 mt-11 mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Through our vision of "Learn, Build and Scale" we implement various initiatives and events in KIET to foster entrepreneurial minds and create a culture of enthralling startups bound for success!</p>
+                <p className=" ml-0 md:ml-52 mr-0 md:mr-52 mt-11 mb-6 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl">Through our vision of "Learn, Build and Scale" we implement various initiatives and events in KIET to foster entrepreneurial minds and create a culture of enthralling startups bound for success!</p>
             </div>
 
 
             <div className="flex flex-wrap mx-auto my-5 ">
-                <div className="w-80 m-auto max-w-sm ">
+                <div className="w-80 mx-auto max-w-sm ">
                     <img src={card1} alt="" className="rounded-t-2xl shadow-2xl lg:w-full 2xl:w-full 2xl:h-44 object-cover" />
                     <div className="bg-white shadow-2xl rounded-b-3xl  w-[86%] md:w-[100%]">
                         <h2 className="text-center text-gray-800 text-2xl font-bold pt-6">Ideate</h2>
@@ -26,16 +26,17 @@ const Cards = () => (
                     </div>
                 </div>
 
-                <div className="w-80 m-auto max-w-sm mt-10 md:mt-0">
+                <div className="w-80 mx-auto max-w-sm mt-10 md:mt-0">
                     <img src={card2} alt="" className="rounded-t-2xl shadow-2xl lg:w-full 2xl:w-full 2xl:h-44 object-cover" />
                     <div className="bg-white shadow-2xl rounded-b-3xl  w-[86%] md:w-[100%]">
                         <h2 className="text-center text-gray-800 text-2xl font-bold pt-6">Create</h2>
                         <div className=" w-5/6 m-auto">
                             <p className="text-center text-gray-500 pt-5 pb-5">Create an entrepreneurship community where we ideate, innovate and mold your idea into a full-fledged running venture.</p>
-                       <br />
+                            <br />
                         </div>
                     </div>
-                </div><div className=" w-80 m-auto max-w-sm mt-10 md:mt-0">
+                </div>
+                <div className=" w-80 mx-auto max-w-sm mt-10 md:mt-0">
                     <img src={card3} alt="" className="rounded-t-2xl shadow-2xl lg:w-full 2xl:w-full 2xl:h-44 object-cover" />
                     <div className="bg-white shadow-2xl rounded-b-3xl  w-[86%] md:w-[100%]">
                         <h2 className="text-center text-gray-800 text-2xl font-bold pt-6">Incubate</h2>
--- src/components/Venture.jsx
+++ src/components/Venture.jsx
@@ -4,23 +4,25 @@ import venture from '../assets/venture.png'
 const Venture = () => (
     <section className="bg-white mt-0 md:mt-10 ">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
-            <div className="lg:mt-0 lg:col-span-5 lg:flex">
+            <div className="lg:mt-0 lg:col-span-5 lg:flex mx-auto">
                 <img src={venture} alt="mockup" />
             </div>
 
             <div className="ml-auto place-self-center lg:col-span-7 ">
-                <h1 className="max-w-2xl mb-4 text-4xl font-light tracking-tight leading-none md:text-5xl xl:text-6xl ">Start your <span className='text-[#4d55bb] font-bold'>Venture!</span></h1>
+                <h1 className="max-w-2xl mb-4 text-4xl font-light tracking-tight leading-none md:text-5xl xl:text-6xl text-center sm:text-left">Start your <span className='text-[#4d55bb] font-bold'>Venture!</span></h1>
 
-                <p className="max-w-xl mt-11 mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl"> <span className='font-medium'>Starting a new business? </span>Find out where to begin and how to achieve success. Your business plan is the foundation of your business. It's a roadmap for how to structure, run, and grow your new business as a whole.</p>
+                <p className="max-w-xl mt-11 mb-6 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl text-center sm:text-left"> <span className='font-medium'>Starting a new business? </span>Find out where to begin and how to achieve success. Your business plan is the foundation of your business. It's a roadmap for how to structure, run, and grow your new business as a whole.</p>
 
+                <div className='mx-auto'>
 
-                <a href="#" className="inline-flex items-center justify-center px-7 py-3 text-xl text-center text-[#4d55bb] border border-[#4d55bb] rounded-lg hover:text-white hvr-sweep-to-right ">
-                    Get Started!
-                </a>
-                <a href="#" className="inline-flex  ml-5 items-center justify-center px-7 py-3 text-xl text-center text-[#4d55bb] border border-[#4d55bb] rounded-lg hover:text-white hvr-sweep-to-right ">
-                    Incubator
-                </a>
+                    <a href="#" className="inline-flex items-center justify-center px-7 py-3 text-xl text-center text-[#4d55bb] border border-[#4d55bb] rounded-lg hover:text-white hvr-sweep-to-right ">
+                        Get Started!
+                    </a>
+                    <a href="#" className="inline-flex  ml-5 items-center justify-center px-7 py-3 text-xl text-center text-[#4d55bb] border border-[#4d55bb] rounded-lg hover:text-white hvr-sweep-to-right ">
+                        Incubator
+                    </a>
 
+                </div>
             </div>
         </div>
     </section>
